098b878f8249a4100e71761714f18136eb9435b7
Recep Inan <recinan.ceng@gmail.com>
2025-09-18 20:23:42 +0300

    Frontend: authService and AuthContext

---
+++ flask-react/frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from "react";
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [token, setToken] = useState(localStorage.getItem("access_token") || null)
+
+    const loginUser = (jwt) => {
+        localStorage.setItem("access_token", jwt);
+        setToken(jwt);
+    }
+
+    const logoutUser = () => {
+        localStorage.removeItem("access_token");
+        setToken(null)
+    };
+
+
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth(){
+    return useContext(AuthContext);
+}
---
+++ flask-react/frontend/src/services/authService.js
@@ -0,0 +1,23 @@
+const API_URL = "http://127.0.0.1:5000/auth"
+
+export async function register(email, password) {
+    const options = {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({email,password})
+    }
+    const response = await fetch(`${API_URL}/register`,options)
+    const data = await response.json()
+    return data.message;
+}
+
+export async function login(email, password) {
+    const options = {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({email,password})
+    }
+    const response = await fetch(`${API_URL}/login`,options)
+    const data = await response.json()
+    return response.access_token;
+}
