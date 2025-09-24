24644e16d18ff1b06561fb9218b322a3e22d0bb3
Recep Inan <recinan.ceng@gmail.com>
2025-09-24 21:13:52 +0300

    Backend: app configuration

--- flask-react/backend/app/config.py
+++ flask-react/backend/app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///mydatabase.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY","supersecretjwtkey")
